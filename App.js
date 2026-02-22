1cb19d37745e362a60b7f963b6a8033eb5b1f8c7
Gourav Pattanaik <pattanaik.gourav@gmail.com>
2026-02-22 16:06:24 +0530

    Added index.html

--- App.js
+++ App.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import React from "react";
+import ReactDOM from "react-dom";
+
+let demo = React.createElement(
+  "h1",
+  { style: { color: "green" } },
+  "Welcome to New React Project",
+);
+ReactDOM.render(demo, document.getElementById("root"));
